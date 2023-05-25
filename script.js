b051bebdc2a5f997c7051f21676dcd3cade00fe7
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-25 17:50:05 +0200

    removed old time stamp test

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ console.log('JS Loaded Successfully!');
 
 const clockOuput = document.getElementById('clock-Output');
 
-// => Test Time Stamp
-// const t = new Date().getTime();
-// console.log(t);
 clockCountdown()
 
 function clockCountdown (outputID) {
