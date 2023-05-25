2003a9a96d8e461103cea26f0fb02aa6eb8f4a5f
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-25 16:30:35 +0200

    Added varibales to calculat e each part of time

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const clockOuput = document.getElementById('clock-Output');
 // => Test Time Stamp
 const t = new Date().getTime();
 console.log(t);
+
+clockCountdown()
+
+function clockCountdown (outputID) {
+
+    const months = 00;
+    const days = 00;
+    const hours = 00;
+    const minutes = 00;
+    const seconds = 00;
+    const mseconds = 00;
+}
