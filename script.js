a0fdbf1ad492a0cf612840944d1d0af622bb1604
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-25 16:44:47 +0200

    variable scope for operations and added con.log

--- script.js
+++ script.js
@@ -10,16 +10,20 @@ clockCountdown()
 
 function clockCountdown (outputID) {
 
-    const months = 00;
+    // => Get today's date and time
+    const actualTime = new Date().getTime();
+    // => Set the date we're counting down to
+    const countdownGoal =  new Date("May 26, 2023 09:30:00").getTime();
+    // => Find the distance between now and the count down date
+    const distance = countdownGoal - actualTime;
+
+    const months = Math.floor(distance)
     const days = 00;
     const hours = 00;
     const minutes = 00;
     const seconds = 00;
     const mseconds = 00;
 
-    // => Get today's date and time
-    const actualTime = new Date().getTime();
-    // => Set the date we're counting down to
-    const countdownGoal =  new Date("May 26, 2023 09:30:00").getTime();
-
+    console.log(distance)
+    console.log(`${months}M, ${days}d, ${hours}h, ${minutes}m, ${seconds}s, ${mseconds}ms`)
 }
