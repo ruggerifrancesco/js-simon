f1ed451b018635fbb9da0393a244138be3273b26
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-25 16:30:55 +0200

    Re added time stamp and coundown goal

--- script.js
+++ script.js
@@ -16,4 +16,10 @@ function clockCountdown (outputID) {
     const minutes = 00;
     const seconds = 00;
     const mseconds = 00;
+
+    // => Get today's date and time
+    const actualTime = new Date().getTime();
+    // => Set the date we're counting down to
+    const countdownGoal =  new Date("May 26, 2023 09:30:00").getTime();
+
 }
