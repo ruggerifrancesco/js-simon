833b20bcf95201c76051b04032f9df9d17339e12
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-26 04:27:20 +0200

    Added wy to print in DOM the timer

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 console.log('JS Loaded Successfully!');
 
-const clockOuput = document.getElementById('clock-Output');
+const clockOutputDays = document.getElementById('clockOutputDays');
+const clockOutputHours = document.getElementById('clockOutputHours');
+const clockOutputMinutes = document.getElementById('clockOutputMinutes');
+const clockOutputSeconds = document.getElementById('clockOutputSeconds');
 
 // Timer and 100ms to delay at seconds
 const clockDelay = setInterval(clockCountdown, 1000)
 
-function clockCountdown (outputID) {
+function clockCountdown () {
 
     // => Get today's date and time
     const actualTime = new Date().getTime();
@@ -22,27 +25,33 @@ function clockCountdown (outputID) {
         console.log('DAJE ROMA');
     }
 }
+  
+function convertMsToTime(milliseconds) {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  let days = Math.floor(hours / 24);
+
+  seconds = seconds % 60;
+  minutes = minutes % 60;
+  hours = hours % 24;
+
+  // Update the countdown digits with the calculated values
+  clockOutputDays.textContent = padTo2Digits(days);
+  clockOutputHours.textContent = padTo2Digits(hours);
+  clockOutputMinutes.textContent = padTo2Digits(minutes);
+  clockOutputSeconds.textContent = padTo2Digits(seconds);
+
+  // NOTE: In this code, the values are calculated by 
+  // dividing the distance by the respective unit in 
+  // milliseconds and using the remainder (%) operator 
+  // to extract the remaining time for the smaller units.
+
+  return `${padTo2Digits(days)}d:${padTo2Digits(hours)}h:${padTo2Digits(minutes)}m:${padTo2Digits(
+    seconds,
+  )}s`;
+}
 
 function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  }
-  
-  function convertMsToTime(milliseconds) {
-    let seconds = Math.floor(milliseconds / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = Math.floor(hours / 24);
-  
-    seconds = seconds % 60;
-    minutes = minutes % 60;
-    hours = hours % 24;
-
-    // NOTE: In this code, the values are calculated by 
-    // dividing the distance by the respective unit in 
-    // milliseconds and using the remainder (%) operator 
-    // to extract the remaining time for the smaller units.
-  
-    return `${padTo2Digits(days)}d:${padTo2Digits(hours)}h:${padTo2Digits(minutes)}m:${padTo2Digits(
-      seconds,
-    )}s`;
-  }
+  return num.toString().padStart(2, '0');
+}
